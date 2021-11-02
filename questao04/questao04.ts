d35875ef6ed5bac01fcc12fcd508035105595a60
Hugo <hugomendes@aluno.uema.br>
2021-11-02 13:50:11 -0300

    Fazendo alterações na quarta questão

--- questao04/questao04.ts
+++ questao04/questao04.ts
@@ -11,39 +11,43 @@ let listaElementos: Array<Object> = [
     { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }];
 
 // Letra A
-let tbody: Element = document.getElementById('tabela')!;
-let elementoAlterado: number = 0;
 
+// Selecionando a tabela com o seletor css(tabela)
+let tbody: Element = document.getElementById('tabela')!;
+
+
+// função que mostra os dados do vetor na tabela HTML
 function listaTabela() {
 
-
-  
-
     for (let i: number = 0; i < listaElementos.length; i++) {
-
+        // Toda vez que o loop for executado a função vai criar todos esses elementos tr e td
         let tr: Element = document.createElement('tr');
         let td_id: Element = document.createElement('td');
         let td_name: Element = document.createElement('td');
         let td_bio: Element = document.createElement('td');
         let td_btn: Element = document.createElement('td');
 
+        // Atribuindo uma classe para cada elemento para que seja possivel manipular o css do button e da linha
+        td_id.setAttribute('id', 'id_input');
         td_id.classList.add('td_id');
         td_name.classList.add('td_name');
         td_bio.classList.add('td_bio');
 
+        // Atribuindo os dados do vetor a cada elemento da linha
         td_id.innerText = listaElementos[i].id;
         td_name.innerText = listaElementos[i].name;
         td_bio.innerText = listaElementos[i].bio;
+        // atribuindo um button ao elemento HTML, assim como a classe para o css
         td_btn.innerHTML = '<button class="btn-apagar" type="button" value="delete" onclick"deleteRow(this)">Excluir</button>';
 
 
-  
+        // Atribuindo cada elemento como filho de tr.
         tr.appendChild(td_id);
         tr.appendChild(td_name);
         tr.appendChild(td_bio);
         tr.appendChild(td_btn);
 
-
+        // Atribuindo o tr como filho da tabela.
         tbody.appendChild(tr);
 
     }
@@ -56,29 +60,28 @@ listaTabela();
 
 // Letra B
 
-
+// Pegando os dados passados pelo formulário
 let inputId: Element = document.getElementById('id')!;
 let inputName: Element = document.getElementById('name')!;
 let inputBio: Element = document.getElementById('bio')!;
 let button: Element = document.getElementById('enviar')!;
 
+// Função que capta o evento de click no botão do formulário
 button.addEventListener('click', () => {
+    let td_inputID = document.querySelectorAll('#id_input')!;
+    let td_inputName = document.querySelectorAll('.td_name')!;
+    let td_inputBio = document.querySelectorAll('.td_bio')!;
+
    
     
-    for(let i: number = 0; i< listaElementos.length; i++){
-        if(inputId.value == listaElementos[i].id){
-            console.log(listaElementos[i].id);
-            let td_inputID = document.querySelector('.td_id')!;
-         
-            td_inputID.innerHTML = inputId.value;
-
-           let td_inputName = document.querySelector('.td_name')!;
-           td_inputName.innerHTML = inputName.value;
-
-           let td_inputBio = document.querySelector('.td_bio')!;
-           td_inputBio.innerHTML = inputBio.value;
-   
+    for(let i: number = 0; i< td_inputID.length; i++){
+        if(inputId.value == td_inputID[i].innerHTML){
+           console.log(td_inputID[i].innerHTML);
+            td_inputID[i].innerHTML = inputId.value;
+            td_inputName[i].innerHTML = inputName.value;
+            td_inputBio[i].innerHTML = inputBio.value;
         }
+
     }
   
 
--- questoes-js/questao04/questao04.js
+++ questoes-js/questao04/questao04.js
@@ -11,45 +11,58 @@ var listaElementos = [
     { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
 // Letra A
+// Selecionando a tabela com o seletor css(tabela)
 var tbody = document.getElementById('tabela');
-var elementoAlterado = 0;
+// função que mostra os dados do vetor na tabela HTML
 function listaTabela() {
     for (var i = 0; i < listaElementos.length; i++) {
+        // Toda vez que o loop for executado a função vai criar todos esses elementos tr e td
         var tr = document.createElement('tr');
         var td_id = document.createElement('td');
         var td_name = document.createElement('td');
         var td_bio = document.createElement('td');
         var td_btn = document.createElement('td');
+        // Atribuindo uma classe para cada elemento para que seja possivel manipular o css do button e da linha
+        td_id.setAttribute('id', 'id_input');
         td_id.classList.add('td_id');
         td_name.classList.add('td_name');
         td_bio.classList.add('td_bio');
+        // Atribuindo os dados do vetor a cada elemento da linha
         td_id.innerText = listaElementos[i].id;
         td_name.innerText = listaElementos[i].name;
         td_bio.innerText = listaElementos[i].bio;
+        // atribuindo um button ao elemento HTML, assim como a classe para o css
         td_btn.innerHTML = '<button class="btn-apagar" type="button" value="delete" onclick"deleteRow(this)">Excluir</button>';
+        // Atribuindo cada elemento como filho de tr.
         tr.appendChild(td_id);
         tr.appendChild(td_name);
         tr.appendChild(td_bio);
         tr.appendChild(td_btn);
+        // Atribuindo o tr como filho da tabela.
         tbody.appendChild(tr);
     }
 }
 listaTabela();
 // Letra B
+// Pegando os dados passados pelo formulário
 var inputId = document.getElementById('id');
 var inputName = document.getElementById('name');
 var inputBio = document.getElementById('bio');
 var button = document.getElementById('enviar');
+// Função que capta o evento de click no botão do formulário
 button.addEventListener('click', function () {
-    for (var i = 0; i < listaElementos.length; i++) {
-        if (inputId.value == listaElementos[i].id) {
-            console.log(listaElementos[i].id);
-            var td_inputID = document.querySelector('.td_id');
-            td_inputID.innerHTML = inputId.value;
-            var td_inputName = document.querySelector('.td_name');
-            td_inputName.innerHTML = inputName.value;
-            var td_inputBio = document.querySelector('.td_bio');
-            td_inputBio.innerHTML = inputBio.value;
+    var td_inputID = document.querySelectorAll('#id_input');
+    var td_inputName = document.querySelectorAll('.td_name');
+    var td_inputBio = document.querySelectorAll('.td_bio');
+    for (var i = 0; i < td_inputID.length; i++) {
+        if (inputId.value == td_inputID[i].innerHTML) {
+            console.log(td_inputID[i].innerHTML);
+            td_inputID[i].innerHTML = inputId.value;
+            td_inputName[i].innerHTML = inputName.value;
+            td_inputBio[i].innerHTML = inputBio.value;
+        }
+        else {
+            inputId.value = '';
         }
     }
 });
